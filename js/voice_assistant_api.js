7c85216786d25d055b8b9ff6ae986b2e40b4eb4f
matemolnar88 <matemolnar88@gmail.com>
2019-07-14 00:13:38 +0200

    API and UI update

--- js/voice_assistant_api.js
+++ js/voice_assistant_api.js
@@ -15,13 +15,13 @@ const weather = [
   'The sun is bright'
 ];
 
-
+// Later task - Remember the allowance settings
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
 // Checking the console log
 recognition.onstart = function(){
-  console.log('Voice assistan API is loaded, ready to start!');
+  console.log('Voice assistan API is loaded, ready to use!');
 };
 
 // Write out the message
@@ -29,7 +29,7 @@ recognition.onresult = function(event){
   const current = event.resultIndex;
 
   const transcript = event.results[current][0].transcript;
-  content.textContent = transcript;
+  content.textContent = transcript + "?";
   readOutLoud(transcript);
 };
 
@@ -41,6 +41,7 @@ btn.addEventListener('click', () => {
 // Read out the message
 function readOutLoud(message){
   const speech = new SpeechSynthesisUtterance();
+  const voices = window.speechSynthesis.getVoices();
   const mistake = 'Sorry, I did\'t catch that. Could you say it again?';
 
   speech.text = mistake;
@@ -51,6 +52,8 @@ function readOutLoud(message){
     speech.text = finalText;
   }
 
+  // the list of the available voices - https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
+  speech.voice = voices[1];
   speech.volume = 1;
   speech.rate = 1;
   speech.pitch = 1;
